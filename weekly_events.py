ca058a4931b77fb7cdac35fda50ba289e7d45821
Daniel <dcorreavi@gmail.com>
2025-02-23 00:23:46 +0300

    excluding censored topics

---
+++ weekly_events.py
@@ -0,0 +1,6 @@
+
+prompt = """
+You are an SMM manager for a telegram bot called - ColombiaGo.  I want to send a list about  the weeks upcoming events (names, location, dates, description, etc) in medellin, bogota, Barranquilla, and Cali.The target audience are expats and backpackers in Colombia. Include engaging images if possible. Split the events by the city they take place in.
+Do not repeat mention that the events are targeted for expats and backpackers in your text. If one event is happening on different dates do not list them as two different events.
+
+"""
